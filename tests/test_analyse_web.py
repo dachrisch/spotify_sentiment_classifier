fff548a62236f02fd950a276bc104221114b6d9e
Christian Dähn <christian.daehn@gmail.com>
2020-05-08 20:06:41 +0200

    test for analyse button

--- tests/test_analyse_web.py
+++ tests/test_analyse_web.py
@@ -1,8 +1,10 @@
 import unittest
 
+from bs4 import BeautifulSoup
 from flask_dance.consumer.storage import MemoryStorage
 
 from app import create_app
+from classify.sentiment import Sentiment
 from fixures.spotify import SpotifyAuthentificationTestService
 from web.views import HomeView
 
@@ -20,6 +22,24 @@ class TestSentimentAnalyseApi(unittest.TestCase):
         response = self.test_client.get('/', follow_redirects=False)
         self.assertEqual(200, response.status_code)
         self.assertIn(b'<h1>Sentiment player for your favorite music</h1>', response.data)
+
+    def test_button_active_when_not_analysed(self):
+        response = self.test_client.get('/', follow_redirects=False)
+        self.assertEqual(200, response.status_code)
+        soup = BeautifulSoup(response.data)
+        button = soup.find(id='btn_analyse')
+        self.assertIn('Analyse your music library', button.next)
+
+    def test_button_deactivated_when_analysed(self):
+        for sentiment in Sentiment:
+            HomeView.service.with_token(None).playlist_manager.add_tracks_to_playlist(('2p9RbgJwcuxasdMrQBdDDA3p',),
+                                                                                      sentiment)
+
+        response = self.test_client.get('/', follow_redirects=False)
+        self.assertEqual(200, response.status_code)
+        soup = BeautifulSoup(response.data)
+        button = soup.find(id='btn_analyse')
+        self.assertIn('Analyse your music library', button.next)
 
 
 if __name__ == '__main__':
