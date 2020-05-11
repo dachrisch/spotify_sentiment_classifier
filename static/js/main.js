cc77bba64ef1dc25e9d1decf68944ce95549f53e
Christian Dähn <christian.daehn@gmail.com>
2020-05-11 14:43:07 +0200

    removed poptrox

--- static/js/main.js
+++ static/js/main.js
@@ -87,26 +87,6 @@
 		// Scrolly.
 			$('.scrolly').scrolly();
 
-		// Poptrox.
-			$window.on('load', function() {
-
-				var $thumbs = $('.thumbnails');
-
-				if ($thumbs.length > 0)
-					$thumbs.poptrox({
-						onPopupClose: function() { $body.removeClass('is-covered'); },
-						onPopupOpen: function() { $body.addClass('is-covered'); },
-						baseZIndex: 10001,
-						useBodyOverflow: false,
-						overlayColor: '#222226',
-						overlayOpacity: 0.75,
-						popupLoaderText: '',
-						fadeSpeed: 500,
-						usePopupDefaultStyling: false,
-						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-					});
-
-			});
 
 		// Initial scroll.
 			$window.on('load', function() {
